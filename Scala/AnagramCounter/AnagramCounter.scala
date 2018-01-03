5b9546cc0bd7119c1be49d779be3c8602d662a98
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2018-01-02 21:42:13 -0700

    implementing anagram counter. todo function return statement

---
+++ Scala/AnagramCounter/AnagramCounter.scala
@@ -0,0 +1,37 @@
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+
+object AnagramCounter	{
+	def reduceDuplicates(args: (String, String))	{
+		val key = args._1
+		val value = args._2
+
+		val valueArray = value.split(" ")
+		var valueSet = valueArray.toSet
+
+		var valueString = ""
+		for(element <- valueSet)	{
+			valueString = valueString + element
+		}
+
+		println(key, valueString)
+
+	}
+
+	def main(args: Array[String])	{
+		val inputFile = args(0)
+		val outputFile = args(1)
+		val conf = new SparkConf().setAppName("anagram counter")		
+		val sc = new SparkContext(conf)
+
+		val inputData = sc.textFile(inputFile)
+		val rdd = inputData.flatMap(line => line.split(" "))
+		val anagramMap = rdd.map(x => (x.sorted, x))
+		val anagramMapTuple = anagramMap.reduceByKey((x,y) => x+","+y)
+		val reduceDup = anagramMap.map(x => reduceDuplicates(x))
+
+
+		result.saveAsTextFile(outputFile)
+
+	}
+}
