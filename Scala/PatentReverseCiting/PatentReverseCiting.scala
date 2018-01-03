7d6c58fe37b44c717dbde82e0d08a7ac665305ac
Sahil Rajesh Dhayalkar <sdhayalk@asu.edu>
2018-01-02 21:41:25 -0700

    implemented patent reverse citing in scala

---
+++ Scala/PatentReverseCiting/PatentReverseCiting.scala
@@ -0,0 +1,18 @@
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+
+object PatentReverseCiting	{
+	def main(args: Array[String])	{
+		val inputFile = args(0)
+		val outputFile = args(1)
+		val conf = new SparkConf().setAppName("patent reverse citing")
+		val sc = new SparkContext(conf)
+
+		val inputData = sc.textFile(inputFile).filter(x => x != "\"CITING\",\"CITED\"")
+		val rdd = inputData.map(line => line.split(","))
+		val rddReverseTuple = rdd.map(x => (x(1), x(0)))
+		val result = rddReverseTuple.reduceByKey((cite1, cite2) => cite1 + ',' + cite2)
+
+		result.saveAsTextFile(outputFile)
+	}
+}
